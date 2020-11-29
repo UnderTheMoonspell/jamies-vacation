4ede8e4ff6736b5a1203e81d72babb8dad24fe67
Bernardo <gsdg>
2020-11-29 02:32:56 +0100

    Updates

---
+++ precache-manifest.5fae8b4f6235c01ef4c206d5fc98fba1.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "801d04b41f21b11fa5c1e5afa7119f95",
+    "url": "/jamies-vacation/index.html"
+  },
+  {
+    "revision": "cc1cbfc70bd18188b93d",
+    "url": "/jamies-vacation/static/css/3.b7f3b785.chunk.css"
+  },
+  {
+    "revision": "1168cf5c07a72a1b6210",
+    "url": "/jamies-vacation/static/css/4.e1c601be.chunk.css"
+  },
+  {
+    "revision": "1a0e1d340991118bba55",
+    "url": "/jamies-vacation/static/css/main.00bdff4b.chunk.css"
+  },
+  {
+    "revision": "2f10bba76c56ca9b750b",
+    "url": "/jamies-vacation/static/js/2.8f7176a1.chunk.js"
+  },
+  {
+    "revision": "daea0ace4f1b9190481ae76e7b9b72d1",
+    "url": "/jamies-vacation/static/js/2.8f7176a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc1cbfc70bd18188b93d",
+    "url": "/jamies-vacation/static/js/3.da3ced59.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/jamies-vacation/static/js/3.da3ced59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1168cf5c07a72a1b6210",
+    "url": "/jamies-vacation/static/js/4.a5c2642c.chunk.js"
+  },
+  {
+    "revision": "1a0e1d340991118bba55",
+    "url": "/jamies-vacation/static/js/main.b425a6ce.chunk.js"
+  },
+  {
+    "revision": "795583769292b25173fc",
+    "url": "/jamies-vacation/static/js/runtime-main.e31f6849.js"
+  },
+  {
+    "revision": "5b747391ddc92460634654aae5f6ea88",
+    "url": "/jamies-vacation/static/media/amsterdam.5b747391.jpg"
+  },
+  {
+    "revision": "13685372945d816a2b474fc082fd9aaa",
+    "url": "/jamies-vacation/static/media/brand-icons.13685372.ttf"
+  },
+  {
+    "revision": "3debcaf1a98e4445d213fd0089d42f6b",
+    "url": "/jamies-vacation/static/media/brand-icons.3debcaf1.svg"
+  },
+  {
+    "revision": "a06da7f0950f9dd366fc9db9d56d618a",
+    "url": "/jamies-vacation/static/media/brand-icons.a06da7f0.woff2"
+  },
+  {
+    "revision": "c1868c9545d2de1cf8488f1dadd8c9d0",
+    "url": "/jamies-vacation/static/media/brand-icons.c1868c95.eot"
+  },
+  {
+    "revision": "ec3cfddedb8bebd2d7a3fdf511f7c1cc",
+    "url": "/jamies-vacation/static/media/brand-icons.ec3cfdde.woff"
+  },
+  {
+    "revision": "2b3e1a91c63ec2bec446943733028395",
+    "url": "/jamies-vacation/static/media/budapest.2b3e1a91.jpg"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/jamies-vacation/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "1ab236ed440ee51810c56bd16628aef0",
+    "url": "/jamies-vacation/static/media/icons.1ab236ed.ttf"
+  },
+  {
+    "revision": "a0369ea57eb6d3843d6474c035111f29",
+    "url": "/jamies-vacation/static/media/icons.a0369ea5.eot"
+  },
+  {
+    "revision": "ad912fd102f4052d3273dd838e0c671d",
+    "url": "/jamies-vacation/static/media/icons.ad912fd1.svg"
+  },
+  {
+    "revision": "b15db15f746f29ffa02638cb455b8ec0",
+    "url": "/jamies-vacation/static/media/icons.b15db15f.woff2"
+  },
+  {
+    "revision": "bea989e82b07e9687c26fc58a4805021",
+    "url": "/jamies-vacation/static/media/icons.bea989e8.woff"
+  },
+  {
+    "revision": "3ba5be59101deffd4aba9b0a7e7adbf9",
+    "url": "/jamies-vacation/static/media/madrid.3ba5be59.jpg"
+  },
+  {
+    "revision": "261d666b0147c6c5cda07265f98b8f8c",
+    "url": "/jamies-vacation/static/media/outline-icons.261d666b.eot"
+  },
+  {
+    "revision": "89f9806b964f92ad282fd60947eba588",
+    "url": "/jamies-vacation/static/media/outline-icons.89f9806b.svg"
+  },
+  {
+    "revision": "c20b5b7362d8d7bb7eddf94344ace33e",
+    "url": "/jamies-vacation/static/media/outline-icons.c20b5b73.woff2"
+  },
+  {
+    "revision": "db78b9359171f24936b16d84f63af378",
+    "url": "/jamies-vacation/static/media/outline-icons.db78b935.ttf"
+  },
+  {
+    "revision": "f89ea91ecd1ca2db7e09baa2c4b156d1",
+    "url": "/jamies-vacation/static/media/outline-icons.f89ea91e.woff"
+  }
+]);
